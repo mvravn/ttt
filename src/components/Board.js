03580f9f932ef58dc3cc3806bdc1c36dd733b886
mvravn <migrti@gmail.com>
2018-11-23 09:35:49 +0100

    progress

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,25 +19,22 @@ class Board extends Component {
   render() {
     console.log(this.props.board);
 
+    const tiles = this.props.board;
+    const boardLayout = tiles.map(tile => {
+      /* console.log(tile); */
+      var piece = ' ';
+      if (tile.occupant === 1) { piece = 'X' }
+      else if (tile.occupant === 2) { piece = 'O' }
+      
+      return (
+        <div className="tile surface" id={`tile_${tile.tileIndex}`} onClick={() => this.handleClick(tile.tileIndex)}>{piece}</div>
+      )
+    })
 
     return (
       <div className="board">
-        {/* <div className="tile surface" id="tile_0" onClick={this.handleClick}>&nbsp;X</div> */}
-        {/* Bør loopes ud i stedet for at blive hardcoded */}
-        {/* 
-        <div className="tile surface" id="tile_0" onClick={() => this.handleClick(0)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_1" onClick={() => this.handleClick(1)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_2" onClick={() => this.handleClick(2)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_3" onClick={() => this.handleClick(3)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_4" onClick={() => this.handleClick(4)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_5" onClick={() => this.handleClick(5)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_6" onClick={() => this.handleClick(6)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_7" onClick={() => this.handleClick(7)}>&nbsp;X</div>
-        <div className="tile surface" id="tile_8" onClick={() => this.handleClick(8)}>&nbsp;X</div> 
-        https://stackoverflow.com/questions/10179815/get-loop-counter-index-using-for-of-syntax-in-javascript
-        */}
-        {this.props.board.forEach((value, i) => console.log('tileIndex: ', this.props.board[i]))}
-
+        {/* https://stackoverflow.com/questions/10179815/get-loop-counter-index-using-for-of-syntax-in-javascript */}
+        { boardLayout }
       </div>
     )
   }
