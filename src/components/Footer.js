08c2279adbd7195420cf83e8c12cd0710fc8dd18
mvravn <migrti@gmail.com>
2018-11-29 15:19:50 +0100

    transfer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 export class Footer extends Component {
+
+  /* handleClick = () => {
+    this.props.changeIcon()
+  } */
+
   render() {
     return (
       <React.Fragment>
         <div className="footer surface">Footer text</div>
 
         <div className="tile surface"></div>
+        {/* <div className="tile surface" id="iconPicker" onClick={() => this.handleClick(icon)}>this.props</div> */}
         <div className="tile surface"></div>
         <div className="tile surface"></div>
       </React.Fragment>
     )
   }
 }
+/* 
+const mapStateToProps = (state) => {
+  return {
+    icon: state.icon.icon
+  }
+}
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeIcon: (icon) => { dispatch ({type: 'CHANGE_ICON', icon: icon});}
+  }
+}
+*/
 export default Footer
